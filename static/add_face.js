17071184fdf81ff7279bc4c22753ed70353b0bcf
NathanielBH <97924767+NathanielBH@users.noreply.github.com>
2023-04-30 12:30:24 -0400

    Add files via upload

--- static/add_face.js
+++ static/add_face.js
@@ -1,20 +1,24 @@
-var button = $("#add_button");
-var message = $("#message");
-var homeButton = $("#home");
-button.click(function() {
-    console.log(button.text());
-    if (button.text() === "Add Face") {
-        $.ajax({
-            url: "/add_face",
-            type: "post",
-            success: function(response) {
-                console.log(response);
-                button.text("Scanning");
-                message.text("Scanning in progress...");
-            }
-        });
+$(document).ready(function() {
+    var button = $("#add_button");
+    var message = $("#message");
+    var homeButton = $("#home");
 
-    } else {
+    button.click(function() {
+        console.log(button.text());
+        if (button.text() === "Add Face") {
+            $.ajax({
+                url: "/add_face",
+                type: "post",
+                success: function(response) {
+                    console.log(response);
+                    button.text("Scanning");
+                    message.text("Scanning in progress...");
+                }
+            });
+        } else if (button.text() === "Scanning") {
+            message.text("Face Scan Successful");
+            button.text("Add Face");
+        } else {
         $.ajax({
             url: "/stop_add",
             type: "post",
@@ -27,6 +31,32 @@ button.click(function() {
     }
 });
 
-homeButton.click(function(){
-    window.location.href = "/home_page.html";
-})
+    $('#add_form2').submit(function(event) {
+        event.preventDefault();
+        var name = $('#fname').val();
+        $.ajax({
+            type: 'POST',
+            url: '/add_face',
+            data: JSON.stringify({'name': name}),
+            contentType: 'application/json',
+            success: function(response) {
+                $('#message').text(response.message);
+            }
+        });
+    });
+
+    homeButton.click(function(){
+        console.log(homeButton.text())
+        if (homeButton.text() === "Home") {
+            $.ajax({
+                url: "/",
+                type: "post",
+                success: function(response) {
+                    console.log(response);
+                }
+            });
+        }
+    });
+});
+
+
